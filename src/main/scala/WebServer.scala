a7c42b4d18588cd33e68a50fc876777a23f50397
William A. O. Brown <will.b611@gmail.com>
2016-07-15 21:17:17 +0100

    Working fibonacci local webserver. Fails for big n, as int is used instead of BigInteger

--- src/main/scala/WebServer.scala
+++ src/main/scala/WebServer.scala
@@ -1,12 +1,14 @@
-import com.twitter.util.Await
+import com.twitter.util.{Await, Future}
 import java.net.InetSocketAddress
+
+import algorithms.sequences.{FibonacciFinder, MatricesFibonacciFinder}
 import io.finch._
 import io.finch.request._
 import io.finch.response._
 import com.twitter.finagle._
 import com.twitter.finagle.Httpx
 import com.twitter.finagle.httpx._
-import com.twitter.finagle.httpx.path._
+import com.twitter.finagle.httpx.path.{Path, _}
 
 object WebServer extends App {
 
@@ -20,27 +22,42 @@ object WebServer extends App {
 object MyEndpoint extends Endpoint[HttpRequest, HttpResponse] {
 
   def route = {
-    case Method.Get -> Root / "hello" / name =>
-      //curl 'http://localhost:9090/hello/Andrea'
-      Service.mk(req =>
-        Ok(s"Hello ${name}").toFuture
-      )
-    case Method.Post -> Root / "user" =>
-      //curl -X POST 'http://localhost:9090/user?name=Andrea&age=65'
-      Service.mk(req => {
-        for {
-          name <- RequiredParam("name")(req)
-          age <- RequiredParam("age")(req).map(_.toInt)
-        } yield {
-          val user = User(name, age)
-          Ok(s"Hello ${user.greet}")
-        }
-      })
+//    case Method.Get -> Root / "sequences" / String =>
+//      //curl 'http://localhost:9090/hello/Andrea'
+//      Service.mk(req =>
+//        Ok(s"Skipping sequence ${}").toFuture
+//      )
+    case Method.Get -> Root / "sequences" / "fibonacci" => SequenceEndPoint
+
+//    case Method.Post -> Root / "user" =>
+//      //curl -X POST 'http://localhost:9090/user?name=Andrea&age=65'
+//      Service.mk(req => {
+//        for {
+//          name <- RequiredParam("name")(req)
+//          age <- RequiredParam("age")(req).map(_.toInt)
+//        } yield {
+//          val user = User(name, age)
+//          Ok(s"Hello ${user.greet}")
+//        }
+//      })
     case _ -> path =>
       Service.mk(req =>
         BadRequest(s"Service not found for path: ${path.toString.replace(Root.toString, "")}").toFuture
       )
   }
+}
 
+object SequenceEndPoint extends Endpoint[HttpRequest, HttpResponse] {
+  val fibonacciFinder: FibonacciFinder = new MatricesFibonacciFinder()
 
+  override def route = {
+    case Method.Get -> Root / "sequences" / "fibonacci" => Service.mk(req => {
+        for {
+          termNumber <- RequiredParam("n")(req).map(_.toInt)
+        } yield {
+          val result = fibonacciFinder.findNthTermInSequence(termNumber).intValue()
+          Ok(s"$result")
+        }
+    })
+  }
 }
