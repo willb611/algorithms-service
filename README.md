381ad8111f7d90506baf79d5b247cea480d1d506
William A. O. Brown <will.b611@gmail.com>
2017-12-13 22:13:07 +0000

    Add example request, add print statement logging port number orn startup

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # algorithms-service
 Scala RPC web service for some of the solutions in the [Algorithms repository](https://github.com/willb611/algorithms).
 Uses [finch](https://github.com/finagle/finch) and [finagle](https://github.com/twitter/finagle).
+
+## Example call
+`http://localhost:9090/sequences/fibonacci?termNumber=9`
--- src/main/scala/WebServer.scala
+++ src/main/scala/WebServer.scala
@@ -7,16 +7,17 @@ import services.{BinomialService, FibonacciSequenceService}
 import io.finch.route.Get
 
 object WebServer extends App {
-
+  val portNumber: Int = 9090
   val binomialService = Get /  "binomial" / "coefficient" /> BinomialService
   val sequenceService = Get / "sequences" / "fibonacci" /> FibonacciSequenceService
 
   val myEndpoint: Service[Request, Response] =
     (binomialService :+: sequenceService).toService
 
+  println(s"Starting server at localhost port: $portNumber")
   Await.ready(
     Httpx.serve(
-      new InetSocketAddress("localhost", 9090),
+      new InetSocketAddress("localhost", portNumber),
       myEndpoint))
 
 }
